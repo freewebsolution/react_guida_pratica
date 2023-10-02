9eecb921c029048d7daf15b0c20dae3a1e751f3f
Luciano <luciano@standupx.com>
2023-10-02 16:17:17 +0200

    Add sez 03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function AppTitle() {
+  return <h1>Todo App</h1>;
+}
+
+const defaultImage = "https://via.placeholder.com/32x32.png";
+
+function getiamgeOrDeafault(imageurl){
+  try{
+    new URL(imageurl);
+    return imageurl;
+  }catch {
+    return defaultImage;
+  }
+}
+
+const user = {
+  name: "Lucio",
+  id:1,
+  image:"https://github.com/freewebsolution.png"
+}
+
+function AppMessage({user:{id,name,image}}) {
+
+  return <p>
+    <span className='welcome'>{id}</span>{" "}
+    <span className='name'>{name}</span>
+    <img src={getiamgeOrDeafault(image)} width="32" alt='user.name' height="32"/>
+  </p>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppTitle />
+      <AppMessage user={user}/>
     </div>
   );
 }
+
+
 
 export default App;
